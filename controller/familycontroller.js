1657e1a54f956fb7e7a627a4d8e53fe74aa91faa
bhautik-baraiya <bhautikbaraiya126@gmail.com>
2024-08-22 19:38:37 +0530

    first commit

---
+++ controller/familycontroller.js
@@ -0,0 +1,61 @@
+var family = require("../model/family");
+
+exports.AddFamily = async (req, res) => {
+  try {
+    const { name, age, contact, relation } = req.body;
+    if ((!name || !age || !contact || !relation)) {
+      res.json({
+        message: "Value is required",
+      });
+
+    } else {
+      const familyData = await family.create(req.body);
+      res.json({
+        message: "User created successfully",
+        data: familyData,
+      });
+    }
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+};
+
+exports.getFamily = async (req, res) => {
+  try {
+    var getLstfamily = await family.find();
+    res.json({
+      message: "user get successful",
+      data: getLstfamily,
+    });
+  } catch (err) {
+    res.json({
+      message: err,
+    });
+  }
+};
+
+
+exports.delFamily = async (req, res) => {
+  try {
+    const delUser = await family.findByIdAndDelete(req.params.id);
+    console.log(delUser);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.editFamily = async (req, res) => {
+  try {
+    console.log(req.body)
+    const ediUser = await family.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    console.log(ediUser);
+    res.json({
+      message: 'Family updated'
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var familycontroller = require("../controller/familycontroller");
+/* GET home page. */
+router.post('/addfamily', familycontroller.AddFamily)
+router.get('/getfamily', familycontroller.getFamily)
+router.post('/delfamily/:id', familycontroller.delFamily)
+router.post('/editfamily/:id', familycontroller.editFamily)
+
+
+module.exports = router;
